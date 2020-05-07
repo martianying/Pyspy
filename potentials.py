43af56c6fda6bdbab29e88bd42d75e227e0ba1bb
martianying <46987628+martianying@users.noreply.github.com>
2020-05-07 23:37:20 +0900

    Update potentials.py

--- potentials.py
+++ potentials.py
@@ -23,24 +23,24 @@ def haloPot(xi, yi, zi, ti, q, span):
 	ti = ti * gyrTos
 
 	SA = 1. - haloGrow(ti / gyrTos, span) * q
-	const = Mhalo * Gcode * HaloCi
+	const = MHALO * Gcode * HALOCI
 
-	xpi = xi * np.cos( omeH * ti) - yi * np.sin( omeH * ti)
-	ypi = xi * np.sin( omeH * ti) + yi * np.cos( omeH * ti)
+	xpi = xi * np.cos( OMEH * ti) - yi * np.sin( OMEH * ti)
+	ypi = xi * np.sin( OMEH * ti) + yi * np.cos( OMEH * ti)
 
-	rratio = np.sqrt(( ypi**2 + ( xpi**2 + zi**2) * SA**2) / (rhalo**2 * SA**2))
+	rratio = np.sqrt(( ypi**2 + ( xpi**2 + zi**2) * SA**2) / (RHALO**2 * SA**2))
 	atanterm = np.arctan( rratio)
 
-	pot = + const * (np.log( rratio) + atanterm / rratio + 0.5 * np.log(( 1. + rratio**2) / ( rratio**2))) / rhalo
+	pot = + const * (np.log( rratio) + atanterm / rratio + 0.5 * np.log(( 1. + rratio**2) / ( rratio**2))) / RHALO
 
 
-	fratio = + const / rhalo * (rratio - np.arctan(rratio)) / ( rratio**3 * rhalo**2)
+	fratio = + const / RHALO * (rratio - np.arctan(rratio)) / ( rratio**3 * RHALO**2)
 
 	fextxpi = - xpi * fratio
 	fextypi = - ypi * fratio / SA**2
 
-	fxi = fextxpi * np.cos( -omeH * ti) - fextypi * np.sin( -omeH * ti)
-	fyi = fextxpi * np.sin( -omeH * ti) + fextypi * np.cos( -omeH * ti)
+	fxi = fextxpi * np.cos( -OMEH * ti) - fextypi * np.sin( -OMEH * ti)
+	fyi = fextxpi * np.sin( -OMEH * ti) + fextypi * np.cos( -OMEH * ti)
 	fzi = - zi * fratio
 
 	return pot, fxi, fyi
@@ -55,10 +55,10 @@ def cgpot(xi, yi, zi, ti):
 	ri = np.sqrt(xi**2 + yi**2 + zi**2)
 
 	As = armEvol(ti / gyrTos)
-	expTerm = np.exp((r0 - np.sqrt( xi**2 + yi**2)) / Rs)
+	expTerm = np.exp((RARM0 - np.sqrt( xi**2 + yi**2)) / RARMS)
 
 	cirVel = addbulge( ri) * kmTom
-	windTerm = cirVel * (ti - tpeak) / ri
+	windTerm = cirVel * (ti - TPEAK) / ri
 
 	xpi = xi * np.cos( windTerm) - yi * np.sin( windTerm)
 	ypi = yi * np.cos( windTerm) + xi * np.sin( windTerm)
@@ -68,15 +68,15 @@ def cgpot(xi, yi, zi, ti):
 	sumPart = 0
 
 	for i in range( len( n_list) ):
-		Kn = n_list[i] * Nm / (np.sqrt( xpi**2 + ypi**2) * np.sin( pA))
-		Bn = Kn * H * (1 + 0.4 * Kn * H)
+		Kn = n_list[i] * NARM / (np.sqrt( xpi**2 + ypi**2) * np.sin( PITCHARM))
+		Bn = Kn * HARM * (1 + 0.4 * Kn * HARM)
 		# no sechTerm here sat simplify the problem
-		Dn = (1 + Kn * H + 0.3 * (Kn * H)**2) / (1 + 0.3 * Kn * H)
-		gamma = Nm * (np.arctan2( ypi, xpi) - np.log(np.sqrt( xpi**2 + ypi**2) / r0) / np.tan(pA))
+		Dn = (1 + Kn * HARM + 0.3 * (Kn * HARM)**2) / (1 + 0.3 * Kn * HARM)
+		gamma = NARM * (np.arctan2( ypi, xpi) - np.log(np.sqrt( xpi**2 + ypi**2) / RARM0) / np.tan(PITCHARM))
 
-		sumPart = sumPart + Cn[i] / ( Kn * Dn) * np.cos(( i + 1) * gamma)
+		sumPart = sumPart + CnARM[i] / ( Kn * Dn) * np.cos(( i + 1) * gamma)
 
-	pot = As * constCG * expTerm * sumPart
+	pot = As * CGARM * expTerm * sumPart
 
 	return pot
 
@@ -90,15 +90,15 @@ def testbar(xi, yi, zi, ti):
 	ti = ti * gyrTos
 
 
-	xpi = xi * np.cos( ti * phibar) - yi * np.sin( ti * phibar)
-	ypi = xi * np.sin( ti * phibar) + yi * np.cos( ti * phibar)
+	xpi = xi * np.cos( ti * PHIBAR) - yi * np.sin( ti * PHIBAR)
+	ypi = xi * np.sin( ti * PHIBAR) + yi * np.cos( ti * PHIBAR)
 
 	arcterm = 2 * np.arctan2( ypi, xpi )
-	five = ( LMabar * beta2)**5
+	five = ( LMABAR * BETA2)**5
 	r = np.sqrt( xi**2 + yi**2 + zi**2)
 	denofive = (1. + r**5 / five)
    
-	const = - barGrowth(ti) * Gcode * Mbar * alpha2 / LMabar**3
+	const = - barGrowth(ti) * Gcode * MBAR * ALPHA2 / LMABAR**3
    
 	d2 = xi**2 + yi**2
 	pot = const * d2 * np.cos(arcterm) / denofive
@@ -107,8 +107,8 @@ def testbar(xi, yi, zi, ti):
 
 	fextypi = const * (5. * ypi * d2 * r**3 * np.cos(arcterm) / (five * denofive**2) - (2. * ypi * np.cos(arcterm) - 2. * xpi * np.sin(arcterm))/denofive)
 
-	fxi = + (fextxpi * np.cos( -phibar * ti) - fextypi * np.sin( -phibar * ti))
-	fyi = + (fextxpi * np.sin( -phibar * ti) + fextypi * np.cos( -phibar * ti))
+	fxi = + (fextxpi * np.cos( -PHIBAR * ti) - fextypi * np.sin( -PHIBAR * ti))
+	fyi = + (fextxpi * np.sin( -PHIBAR * ti) + fextypi * np.cos( -PHIBAR * ti))
 	fzi = const * 5. * zi * d2 * r**3 * np.cos(arcterm) / (five * denofive**2)
 
 	return pot, fxi, fyi
