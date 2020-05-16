b0a7080071407edf036ef3733491c1520b0ab60c
martianying <46987628+martianying@users.noreply.github.com>
2020-05-16 23:44:09 +0900

    Update grid.py

--- grid.py
+++ grid.py
@@ -74,11 +74,12 @@ def sliceDisc(data, theta_loc):
 
 
 def secterArea(loc):
+    #return the quantity in m^2.
     width = RM / RESO_R
     length_up = DIS_THETA[1] * DIS_R[loc +1]
     length_down = DIS_THETA[1] * DIS_R[loc]
     result = 1/ 2 * (length_up + length_down) * width
-    return result
+    return result * kpcTom**2
 
 
 def gridR(data, nq, r_loc, theta_loc):
@@ -111,7 +112,7 @@ def getDenFileOf(raw_data_file):
 
     for i in range(len(DIS_R) - 1):
         test = loadtxt(np_data_file, unpack=True, usecols=[i])
-        den_for_this_col = test / secterArea(i)
+        den_for_this_col = test * PARTICLE_MASS / secterArea(i)
         for k in range(len(den_for_this_col)):
             den_w.write("%f\t" % (den_for_this_col[k]))
         den_w.write("\n")
@@ -149,36 +150,57 @@ def adjustFile(denfile_name, CONTRAST='y'):
     return dendata
 
 
-def plotHeatmap(denFileName):
+def plotHeatmap(denFileName, TITLE):
     import seaborn as sns
-    #here the adjustFile function has already directed the directory.
-    adjusted_denFile = adjustFile(denFileName,'n')
-    sns.heatmap(adjusted_denFile, cmap="Blues")
-    plt.title("Heatmap of grided disc(To be customized)")
-    plt.xlabel("DIS_THETA")
-    plt.ylabel("DIS_R")
-    return plt.show()
+
+    # here the adjustFile function has already directed the directory.
+    adjusted_denFile = adjustFile(denFileName, 'n')
+    # fig, ax = plt.subplots(figsize=(7,5))
+    heat_map = sns.heatmap(adjusted_denFile, cmap="RdPu",
+                           cbar_kws={'label': 'Dnesity Contrast'}, vmin=0, vmax=1700)
+
+    cbar = heat_map.collections[0].colorbar
+    cbar.set_ticks([0, 1700 * 1 / 4, 1700 * 2 / 4, 1700 * 3 / 4, 1700])
+    cbar.set_ticklabels(['0', '25%', '50%', '75%', '100%'])
+    cbar.ax.tick_params(labelsize=8, grid_alpha=0.5, direction='in')
+
+    plt.xticks([0, RESO_THETA, 2 * RESO_THETA, 3 * RESO_THETA, 4 * RESO_THETA],
+               ['0', '$\pi$ / 2', '$\pi$', '$3\pi / 2$', "2$\pi$"], rotation=0)
+
+    plt.yticks([0, RESO_R / RM * 5, RESO_R / RM * 10, RESO_R / RM * 15],
+               ['0', '5', '10', '15'], rotation=0)
+
+    plt.tick_params(direction="in")
+    plt.title(TITLE, x=0.5, y=1.05)
+    plt.xlabel(r"$\theta$ [rad]")
+    plt.ylabel("R [kpc]")
+
+    return plt.savefig('images/' + TITLE, dpi=200), print("finished")
+
 
 
 # plot the surface density along the radius at theta = 0.
-def denAlongR(denfile_path_name, scolor, lab):
-    # figure(dpi=300, facecolor='w', edgecolor='k')
-    f = open(denfile_path_name)
-    denRi = loadtxt(f, unpack=True, usecols=[0])
-    radius = DIS_R[:-1]
+def denAlongR(denfile_path_name_list, scolor_list, lab_list):
+    (figure, axes) = plt.subplots()
+    for i in range(len(denfile_path_name_list)):
+        f = open(denfile_path_name_list[i])
+        denRi = loadtxt(f, unpack=True, usecols=[0])
+        radius = DIS_R[:-1]
 
-    # plt.plot(radius, denRi * 0.01, linewidth=2.5)
-    plt.scatter(radius, denRi * 0.01, color=scolor, s=25, label=lab)
-    plt.yscale("log")
-    #sns.set(style="ticks", palette="muted")
-    plt.xlabel('R [kpc]')
-    plt.ylabel(r'Column Density  [g/$kpc^{2}$]')
-    # plt.ylim(0, y_lim)
 
-    plt.title('Initial Radial Column Density')
-    plt.legend()
+        plt.scatter(radius, denRi, color=scolor_list[i], s=25, label=lab_list[i])
+        plt.yscale("log")
 
-    return plt.savefig(denfile_path_name[:-4] + '.png', dpi=200)
+        plt.xlabel('R [kpc]')
+        plt.ylabel(r'Column Density  [g/$m^{2}$]')
+        plt.tick_params(direction="in")
+
+        axes.yaxis.set_tick_params(direction='in', which='both')
+        plt.ylim(10, 1000000)
+
+        plt.title('Initial Radial Column Density')
+        plt.legend()
+    return plt.savefig(denfile_path_name_list[0][:-4] + '.png', dpi=200), print("go den/ to check your output.")
 
 
 
