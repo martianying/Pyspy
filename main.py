39014d4c1588076a5fa7a17e0c1ec7e8bc08c254
martianying <46987628+martianying@users.noreply.github.com>
2021-05-30 01:09:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,15 @@
-# -----------------IMPORT PACAGES--------------------#
+##################################   PLAYGOURND   ######################################
+# you could play with the code below to generate images I have put into my thesis
+# Unfortunately, I could not provide any data from the research at this moment since it might
+# will be published as a paper in the future.
 
-from potentials import *
-from phycon import *
-from grid import *
-from spiralFit import *
-from coCurve import *
-from growth import *
+import os
+from src.roCurve import *
+from src.potentials import *
 
-#rotationCurve(addbulge, "black", "Rotation Curve", '+')
-os.chdir('/Users/veronicachang/Desktop/pyThesis')
-#getDenFileOf("e7bbulge_00000.txt")
-#plotPot(cgpot, "potential", "CG Contour Plot", 0.0002)
-#---------------get the appendix_pyplot figure in Appendix.-----------
-# denAlongR("den/den_bbulge_00050.txt", "#A569BD","default")
-# denAlongR("den/den_ebbulge_00000.txt", "#C70039", "s = 3 kpc")
-# denAlongR("den/den_e7bbulge_00000.txt", "#7D3C98", "s = 7 kpc")
+# set working directory
+os.chdir('/Users/.../Desktop/...')
 
-
-#---------------get the rotation curve plot.-------------
-# rotationCurve(addbulge, "#4A235A", "Total", '-', "mini4rc.txt", 1)
-# rotationCurve(halo,"#C70039","Halo",'--')
-# rotationCurve(disc, "#3498DB", "Disc", '--')
-# rotationCurve(bulge, "#1ABC9C", "Bulge", '--')
-
-
-#---------------get the contour plot-------------
-#comparePot(subPlotStat(haloPot, "potential", 0.), subPlotStat(haloPot, "potential", 0.5),LABEL1="t = 0 Gyr", LABEL2 = "t = 1 Gyr", TITLE="Halo Contour Plot", VMIN = 5*10**10, VMAX = 8.5*10**10)
-
-#comparePot(subPlotStat(cgpot, "potential", 3), subPlotStat(cgpot, "potential", 3.0005),LABEL1="t = 3 Gyr", LABEL2 = "t = 3.0005 Gyr", TITLE="C&G Arms Contour Plot", VMIN = -8 * 10 ** 8, VMAX = 8 * 10 ** 8)
-#comparePot(subPlotStat(cgpot, "potential", 2.998), subPlotStat(cgpot, "potential", 2.9995),LABEL1="t = 2.998 Myr", LABEL2 = "t = 2.9985 Myr", TITLE="C&G Arms Contour Plot", VMIN = -8 * 10 ** 8, VMAX = 8 * 10 ** 8)
-#comparePot(subPlotStat(testbar, "potential", 0.00000001), subPlotStat(testbar, "potential", 0.08),LABEL1="t = 0 Myr", LABEL2 = "t = 80 Myr", TITLE="Bar Contour Plot", VMIN = -6 * 10 ** 7, VMAX = 6 * 10 ** 7)
+if __name__ == '__main__':
+    # Plot the rotation curve
+    rotationCurve(addbulge, "black", "Rotation Curve", '+')
